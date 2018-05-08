831c606841d84526ae7588463994ed95eb1a06ac
Adarsh <Adarsh@Adarsh-PC>
2018-05-08 23:40:23 +0530

    project structure

---
+++ taskmanager-service/src/test/java/com/taskmanager/taskmanagerservice/TaskmanagerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.taskmanager.taskmanagerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskmanagerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
