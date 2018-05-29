8ed621804d4ef588388a8ebc189b5795b0e9c986
Adarsh <Adarsh@Adarsh-PC>
2018-05-29 22:25:08 +0530

    clean up

---
+++ service-registry/src/main/java/com/example/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,18 @@
+package com.example.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Adarsh
+ *Eureka Server Spring Boot Application
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+}
