b58270410cfdd587fbd41e9e93d5bc6c16858fe2
Adarsh <Adarsh@Adarsh-PC>
2018-06-05 00:01:23 +0530

    centralize common api for service orchestration

---
+++ taskmanager-common/src/main/java/com/common/taskmanager/ne/vo/Notification.java
@@ -0,0 +1,26 @@
+/*
+ * (C) Copyright 
+ * Author :Adarsh , created on 31-May-2018
+ * Notification.java ,
+ *
+ */
+
+package com.common.taskmanager.ne.vo;
+
+/**
+ * @author Adarsh
+ * Notification-wrapper object for notification 
+ * To Do -need to add valid properties removing mock
+ */
+public class Notification {
+	
+private Long notificationId;
+
+public Long getNotificationId() {
+	return notificationId;
+}
+
+public void setNotificationId(Long notificationId) {
+	this.notificationId = notificationId;
+}
+}
